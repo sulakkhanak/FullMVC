e1a10c83b34dd1818c6dceeda2173f8aa6c7745d
sulakkhanak <51056097+sulakkhanak@users.noreply.github.com>
2019-06-06 22:12:13 +0530

    Add files via upload

---
+++ ContactsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sulakkhana_Contacts.Models;
+using Sulakkhana_Contacts.BusinessLayer;
+
+namespace Sulakkhana_Contacts.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private Repository rule;
+
+        private ContactDB_SulakkhanaEntities context = new ContactDB_SulakkhanaEntities();
+
+        public ContactsController()
+        {
+            rule = new Repository(new ContactDB_SulakkhanaEntities());
+        }
+
+        //
+        // GET: /Contacts/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetContacts()
+        {
+            var con = rule.GetContacts().ToList();
+            return Json(con, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult CreateContact(Contacts contact)
+        {
+            return Json(rule.CreateContact(contact), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetCountries()
+        {
+            var CusData = rule.GetCountries().ToList();
+            return Json(CusData, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult UpdateContact(Contacts contact)
+        {
+            return Json(rule.UpdateContact(contact), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult DeleteContact(int ContactID)
+        {
+            return Json(rule.DeleteContact(ContactID), JsonRequestBehavior.AllowGet);
+        }
+	}
+}
---
+++ Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sulakkhana_Contacts.Models;
+
+namespace Sulakkhana_Contacts.BusinessLayer
+{
+    public class Repository
+    {
+        private ContactDB_SulakkhanaEntities context;
+
+        public Repository(ContactDB_SulakkhanaEntities context)
+        {
+            this.context = context;
+        }
+
+        public int CreateContact(Contacts c)
+        {
+            return context.InsertContact(c.Name, c.Address, c.Tel, c.Mobile, c.Email, c.CountryID);
+           
+        }
+
+        public int UpdateContact(Contacts c)
+        {
+            return context.UpdateContact(c.ContactID,c.Name, c.Address, c.Tel, c.Mobile, c.Email, c.CountryID);
+        }
+
+        public IEnumerable<Contacts> GetContacts()
+        {
+            return context.GetContacts().Select(c => new Contacts { ContactID = c.ContactID, Name = c.Name, Address = c.Address, Tel = c.Tel, Mobile = c.Mobile, Email = c.Email, CountryID = (int)c.CountryID,Country=c.Country });
+        }
+
+        public int DeleteContact(int ContactID)
+        {
+            return context.DeleteContact(ContactID);
+        }
+
+        public IEnumerable<Countries> GetCountries()
+        {
+            return context.GetCountries().Select(c => new Countries { CountryID = (int)c.CountryID, Country = c.Country }); ;
+        }
+
+        
+
+    }
+}
